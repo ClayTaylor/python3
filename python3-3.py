06e517c9061a44505e52a5118ce710c2e04968ca
Clay Taylor <claydevelops@gmail.com>
2021-02-01 17:24:15 -0600

    Intro to Logic

---
+++ python3-3.py
@@ -0,0 +1,42 @@
+#Comments
+#This is my 3rd video for the day
+
+'''
+A lot of text
+A lot of text
+A lot of text
+A lot of text
+A lot of text
+A lot of text
+A lot of text
+A lot of text
+A lot of text ''' #Be cautious using these
+
+#I can enable and disable code using comments.
+
+#x = 'Hello World'
+#print(x)
+
+#-------------
+
+#Booleans True or False (like a light switch)
+
+x = True #Notice this is case sensitive
+y = False #Notice this is case sensitive
+print(x, y)
+
+#-------------
+
+#Comparisons are the 'Building Block' of logic
+
+x = 4
+y = 3
+
+#Intro to Logic (If Else)
+
+if x > y :
+    print("X is larger than Y")
+
+else :
+    print("Y is larger than x")
+
