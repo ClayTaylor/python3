763faa5f30f54158172bbb0ea2bbef00a32adbf8
Clay Taylor <claydevelops@gmail.com>
2021-02-01 16:59:57 -0600

    Learning Variables

---
+++ learning-variables.py
@@ -0,0 +1,10 @@
+#What is a Variable
+#Something that WILL change
+#Used to Store Data (Data Types)
+
+x = 'cats'
+print(x[2]) #Prints the variable's 3rd letter.
+
+x = 42
+print(x)
+
--- test.py
+++ test.py
@@ -1,1 +1,3 @@
-print('Hello World')
+x = 5 * 3
+
+print(x)
